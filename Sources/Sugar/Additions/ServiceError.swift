dae2010d410e6bd3f14b6fe7995e5b5068753975
Marton Kerekes <marton.kerekes@adevinta.com>
2021-03-29 20:38:15 +0200

    Error image support

--- Sources/Sugar/Additions/ServiceError.swift
+++ Sources/Sugar/Additions/ServiceError.swift
@@ -1,6 +1,8 @@
 import Foundation
+import SwiftUI
 
 public protocol ServiceError: Error {
     var errorDescription: String { get }
     var recoverySuggestion: String { get }
+    var suggestiveImage: Image { get }
 }
