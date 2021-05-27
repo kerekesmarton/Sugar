d8cf8a3ce838bfbda382d9f46d857a9ff308b406
Marton Kerekes <marton.kerekes@adevinta.com>
2021-05-27 08:33:25 +0200

    SDWebImageRefactor

---
+++ Sources/Sugar/Additions/ImageStorage.swift
@@ -0,0 +1,8 @@
+import Sugar
+
+public protocol ImageStorage {
+    func cachedImage(for model: AsyncImageModel, completion: @escaping (UIImage?) -> Void)
+    func store(_ data: Data, for model: AsyncImageModel, completion: @escaping Action)
+    func erase(_ block: @escaping Action)
+    func downloadImage(with url: URL, completion: @escaping (UIImage?, Data?, Error?, Bool) -> Void)
+}
